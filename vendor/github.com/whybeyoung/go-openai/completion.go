35cfbebbf7e20160f591d631e50f73591147273a
wenli14 <wenli14@iflytek.com>
2026-07-31 14:41:17 +0800

    temperature fix

--- vendor/github.com/whybeyoung/go-openai/completion.go
+++ vendor/github.com/whybeyoung/go-openai/completion.go
@@ -225,7 +225,7 @@ func validateRequestForO1Models(request ChatCompletionRequest) error {
 	}
 
 	// Other: temperature, top_p and n are fixed at 1, while presence_penalty and frequency_penalty are fixed at 0.
-	if request.Temperature > 0 && request.Temperature != 1 {
+	if request.Temperature != nil && *request.Temperature > 0 && *request.Temperature != 1 {
 		return ErrO1BetaLimitationsOther
 	}
 	if request.TopP > 0 && request.TopP != 1 {
